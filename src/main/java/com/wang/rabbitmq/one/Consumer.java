b52ca9224685d7a74473820adc97653c23dd439a
wangyi <1572861540@qq.com>
2023-02-18 12:45:44 +0800

    consumer code success

---
+++ src/main/java/com/wang/rabbitmq/one/Consumer.java
@@ -0,0 +1,44 @@
+package com.wang.rabbitmq.one;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 消费者
+ */
+public class Consumer {
+    //队列的名称
+    public static final String QUEUE_NAME="hello,wang";
+
+    //接受消息
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //创建连接工厂
+        ConnectionFactory factory =new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("admin");
+        factory.setPassword("123");
+        Connection connection =factory.newConnection();
+
+        Channel channel1 =connection.createChannel();
+
+        //声明 接受消息
+        DeliverCallback deliverCallback=(consumerTag,message)->{
+            System.out.println(new String(message.getBody()));
+        };
+        //取消消息时的回调
+        CancelCallback cancelCallback =consumerTag ->{
+            System.out.println("消息消费被中断");
+        };
+        /**
+         * 消费者消费信息
+         * 1.消费哪个队列。
+         * 2.消费成功之后是否要自动应答。
+         * 3.消费者未成功消费的回调
+         * 4.消费者取录消费的回调
+         */
+
+        channel1.basicConsume(QUEUE_NAME,true,deliverCallback,cancelCallback);
+    }
+}
